2d9d473156436dc9c0b83bd0f7505b5fe0f55645
udpa <udpa@undonpouragir.fr>
2016-01-23 13:57:29 +0100

    added table settings

---
+++ src/LesAutres/SiteBundle/Admin/SettingAdmin.php
@@ -0,0 +1,107 @@
+<?php
+namespace LesAutres\SiteBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class SettingAdmin extends Admin
+{
+    protected $translationDomain = 'SonataPageBundle';
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with(
+                'Général',
+                array(
+                    'description' => "",
+                )
+            )
+            ->add(
+                'name',
+                'text',
+                array(
+                    'label'  => "Nom",
+                    'required' => false,
+                )
+            )
+            ->add(
+                'value',
+                'text',
+                array(
+                    'label'  => "Valeur",
+                    'required' => false,
+                )
+            )
+            ->end()
+        ;
+    }
+
+    /*protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'setting_name',
+                null,
+                array(
+                    'label'  => "Nom",
+                )
+            )
+        ;
+    }*/
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label'  => "Nom",
+                )
+            )
+            ->add(
+                'value',
+                null,
+                array(
+                    'label'  => "Valeur",
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        'view' => array(),
+                        'edit' => array(),
+                        'delete' => array(),
+                    )
+                )
+            )
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label'  => "Nom",
+                )
+            )
+            ->add(
+                'value',
+                null,
+                array(
+                    'label'  => "Valeur",
+                )
+            )
+        ;
+    }
+}
---
+++ src/LesAutres/SiteBundle/Entity/Setting.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace LesAutres\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Les\AutresBundle\Entity\Setting
+ *
+ * @ORM\Table(name="settings")
+ * @ORM\Entity(repositoryClass="LesAutres\SiteBundle\Entity\SettingRepository")
+ */
+class Setting
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=128, unique=true, name="setting_key")
+     */
+    private $key;
+
+    /**
+     * @ORM\Column(type="string", length=256, name="setting_name")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=1024, name="setting_value")
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="datetime", name="created_at")
+     */
+    protected $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", name="updated_at")
+     */
+    protected $updatedAt;
+    
+    
+    
+    
+    
+    
+    /**
+     * CONSTRUCTOR
+     */
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+    
+    public function __toString()
+    {
+        return (
+            $this->key ?
+            $this->key :
+            "Nouvelle option"
+        );
+    }
+    
+    
+    
+    
+    
+    
+    /**
+     * GETTERS & SETTERS
+     */
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set key
+     *
+     * @param string $key
+     * @return Setting
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    /**
+     * Get key
+     *
+     * @return string 
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     * @return Setting
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Setting
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Setting
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Setting
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
